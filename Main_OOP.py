10cb505626bc4aef023f1e882071cfc2023d9cab
nickikeeboh <134042606+nickikeeboh@users.noreply.github.com>
2023-11-14 21:32:53 +1030

    Update Main_OOP.py
    Added classes and general information

---
+++ Main_OOP.py
@@ -0,0 +1,33 @@
+'''
+File: Main_OOP.py
+Description: A brief description of this Python module.
+Author: Nick Ikeeboh
+StudentID: 11046
+EmailID: Ikeno001
+This is my own work as defined by the University's Academic Misconduct Policy.
+'''
+
+class Alchemist():
+    pass
+
+class Laboratory():
+    pass
+
+class Potion():
+    pass
+
+class Reagent():
+    pass
+
+class SuperPotion(Potion):
+    pass
+
+class ExtremePotion(Potion):
+    pass
+
+class Herb(Reagent):
+    pass
+
+class Catalyst(Reagent):
+    pass
+
